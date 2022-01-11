87e2e19c05ac501501f152b9c4c5d4469d3d9003
nuthan <inbox.nuthan@gmail.com>
2022-01-11 23:50:58 +0530

    Added another new npm module, added sample code how to use that npm module in index.js

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 console.log("Welcome to world of nodejs");
 
 const superhero = require('superheroes')
-console.log(superhero.random()); // Gives any one random super hero name
+// Gives any one random super hero name
+console.log('Hero Name: ' + superhero.random()); 
 
-console.log(superhero.all); // Gives in string array format
+// Gives all supper hero names in string array format
+console.log(superhero.all); 
+
+// To get super villain name
+const supervillain = require('supervillains')
+
+// Gives any one random super villain name
+console.log('Super villain Name: ' + supervillain.random()); 
+
+// Gives all supper villain names in string array format
+console.log(supervillain.all);
+
