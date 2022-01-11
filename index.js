a3d37548f54c3fecf85161f7771d646218cbb1a5
nuthan <inbox.nuthan@gmail.com>
2022-01-11 23:41:06 +0530

    Added one new file: package-lock.json, added new npm module, added sample code how to use that npm module in index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log("Welcome to world of nodejs");
+
+const superhero = require('superheroes')
+console.log(superhero.random()); // Gives any one random super hero name
+
+console.log(superhero.all); // Gives in string array format
